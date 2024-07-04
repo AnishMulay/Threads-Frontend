9b6055dfc84e71b5318ba7223b771ffe5129d7f5
Ayush-Gala <ayushgala2@gmail.com>
2024-07-04 13:21:02 +0530

    initial commit

---
+++ client/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faLock,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faTwitter,
+  faGoogle,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Login = () => {
+  return (<></>);
+};
+
+export default Login;
